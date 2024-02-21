62631bac399b9b429fcb3807e401240b1bf2bfd2
Bidhan Chandra Roy <bidhan.roy@walmart.com>
2024-02-21 15:36:30 +0530

    origin/main: Initial folder structure commit.

---
+++ WeatherApp/View/CurrentLocationWeatherView.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentLocationWeatherView.swift
+//  WeatherApp
+//
+//  Created by Bidhan Chandra Roy on 21/02/24.
+//
+
+import SwiftUI
+
+struct CurrentLocationWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ WeatherApp/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bidhan Chandra Roy on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+           CurrentLocationWeatherView()
+        }
+    }
+}
